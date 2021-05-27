c8ffa3bc232842576318c4b77176d3bc238c4558
Aji Priyo Wibowo <ajipwch@gmail.com>
2021-05-28 02:12:30 +0700

    First Initialize GUI file

---
+++ gui/__info__.py
@@ -0,0 +1,6 @@
+APP_NAME = "Anime Scrapper Indonesia"
+APP_NAME_NO_SPACE = APP_NAME.replace(' ', '')
+APP_VERSION = '0.1'
+APP_DESCRIPTION = "Browse anime from Indonesia without ads"
+COMPANY_NAME = "Nexiamuel"
+WEBSITE = "https://github.com/ajipw/anime-grabber"
